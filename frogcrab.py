c6566d1c25766d956d1f6cd120f96e566a19d6d4
zom <jonathonwhite5@gmail.com>
2018-08-15 13:53:22 -0400

    plotting individual terms along the time axis

--- frogcrab.py
+++ frogcrab.py
@@ -4,25 +4,43 @@ from scipy.linalg import toeplitz
 
 
 
-def plot_location(tau_index, p_index, axis_t, axis_p, number, product, tau, p):
 
-    axis_t.plot(t, np.real(product[p_index, tau_index, :]), color='blue')
-    axis_t.plot(t, np.imag(product[p_index, tau_index, :]), color='red')
-    axis_t.set_xlabel('time')
+
+def plot_location(tau_index, p_index, axis_t, axis_p, number, product, terms, tau, p):
+
+    axis_t[0][number].plot(t, np.real(product[p_index, tau_index, :]), color='blue')
+    axis_t[0][number].plot(t, np.imag(product[p_index, tau_index, :]), color='red')
+    axis_t[0][number].set_xlabel('time')
     integral_time = dt * np.sum(product[p_index, tau_index, :])
     integral_time = np.abs(integral_time)
-    axis_t.text(0.1, 1.1, 'abs of integral along t: {}'.format(integral_time),
-               transform=axis_t.transAxes, backgroundcolor='white')
-    axis_t.text(0.1, 1.05, 'p_index: {}, p: {}'.format(p_index, p[p_index]),
-               transform=axis_t.transAxes, backgroundcolor='white')
-    axis_t.text(0.1, 1.0, 'tau_index: {}, tau: {}'.format(tau_index, tau[tau_index]),
-               transform=axis_t.transAxes, backgroundcolor='white')
+    axis_t[0][number].text(0.1, 1.1, 'abs of integral along t: {}'.format(integral_time),
+               transform=axis_t[0][number].transAxes, backgroundcolor='white')
+    axis_t[0][number].text(0.1, 1.05, 'p_index: {}, p: {}'.format(p_index, p[p_index]),
+               transform=axis_t[0][number].transAxes, backgroundcolor='white')
+    axis_t[0][number].text(0.1, 1.0, 'tau_index: {}, tau: {}'.format(tau_index, tau[tau_index]),
+               transform=axis_t[0][number].transAxes, backgroundcolor='white')
 
-    axis_t.text(0.1, 0.9, number,
-               transform=axis_t.transAxes, backgroundcolor='white')
+    axis_t[0][number].text(0.1, 0.9, number,
+               transform=axis_t[0][number].transAxes, backgroundcolor='white')
 
     text = axis_p.text(tau[tau_index], p[p_index], number, backgroundcolor='white', alpha=0.5)
     text.set_bbox(dict(facecolor='white', alpha=0.5, edgecolor='black'))
+
+    axis_t[1][number].plot(t, terms['E_xuv_delay'][p_index, tau_index, :], label='E_xuv_delay',
+                           color='blue', alpha=0.5)
+
+    axis_t[1][number].plot(t, terms['d_m'][p_index, tau_index, :], label='d_m',
+                           color='black', alpha=0.5)
+
+    axis_t[1][number].plot(t, terms['exp_phi_p_t'][p_index, tau_index, :], label='exp_phi_p_t',
+                           color='green', alpha=0.5)
+
+    axis_t[1][number].plot(t, terms['e_ft'][p_index, tau_index, :], label='e_ft',
+                           color='red', alpha=0.5)
+
+
+
+
 
 
 
@@ -51,6 +69,7 @@ def plot_streaking_trace(t, E_irt, E_xuv, I_p, dt):
 
     # construction of phi(p, t) term
     phi_p_t = p_m * A_t_reverse_int + 0.5 * A_t_reverse_int_square
+    exp_phi_p_t = np.exp(-1j * phi_p_t)
 
     # construction of delayed XUV matrix
     E_padded = np.pad(E_xuv[::-1], (0, len(E_xuv) - 1), mode='constant')
@@ -72,7 +91,7 @@ def plot_streaking_trace(t, E_irt, E_xuv, I_p, dt):
     e_ft = np.exp(1j * ((p_m**2)/2 + I_p) * t_m)
 
     # multiply all the terms together
-    product = E_xuv_delay * d_m * np.exp(-1j * phi_p_t) * e_ft
+    product = E_xuv_delay * d_m * exp_phi_p_t * e_ft
 
     # integrate the product
     integrated = dt * np.sum(product, 2)
@@ -84,15 +103,21 @@ def plot_streaking_trace(t, E_irt, E_xuv, I_p, dt):
     ax[0][0].set_xlabel('tau')
     ax[0][0].pcolormesh(tau, p, S, cmap='jet')
 
-    # plot visualization aids showing product along time axis before integration
-    plot_location(tau_index=128, p_index=10, axis_t=ax[0][1], axis_p=ax[0][0], number=1, product=product, tau=tau, p=p)
-    plot_location(tau_index=128, p_index=45, axis_t=ax[0][2], axis_p=ax[0][0], number=2, product=product, tau=tau, p=p)
-    plot_location(tau_index=128, p_index=80, axis_t=ax[0][3], axis_p=ax[0][0], number=3, product=product, tau=tau, p=p)
+    # for plotting individual terms
+    terms = {'E_xuv_delay': E_xuv_delay, 'd_m': d_m, 'exp_phi_p_t': exp_phi_p_t, 'e_ft': e_ft}
 
+    # plot visualization aids showing product along time axis before integration
+    plot_location(tau_index=128, p_index=10, axis_t=ax, axis_p=ax[0][0], number=1, product=product, terms=terms, tau=tau, p=p)
+    plot_location(tau_index=128, p_index=45, axis_t=ax, axis_p=ax[0][0], number=2, product=product, terms=terms, tau=tau, p=p)
+    plot_location(tau_index=128, p_index=80, axis_t=ax, axis_p=ax[0][0], number=3, product=product, terms=terms, tau=tau, p=p)
+
+    # plot A(t)
     ax[1][0].plot(t_m[0, 0, :], A_t[0, 0, :], color='blue')
     text = ax[1][0].text(0.1, 0.9, 'A(t)', transform=ax[1][0].transAxes, color='blue')
     text.set_bbox(dict(facecolor='white', alpha=0.5, edgecolor='black'))
     plt.show()
+
+
 
 
 tmax = 30e-15
@@ -102,12 +127,12 @@ t = dt * np.arange(-N/2, N/2, 1)
 
 #define IR pulse
 width_ir = 20e-15
-w0_ir = 2e14
+w0_ir = 5e14
 E_irt = 1e21 * np.real(np.exp(-t**2/width_ir**2)*np.exp(1j * w0_ir * t))
 
 #define xuv pulse
 width_xuv = 0.5e-15
-w0_xuv = 10e15
+w0_xuv = 5e15
 E_xuv = 1 * np.real(np.exp(-t**2/width_xuv**2)*np.exp(1j * w0_xuv * t))
 
 fig, ax = plt.subplots(2, 1)
