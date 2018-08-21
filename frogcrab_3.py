f140ca0cf261e07fd36e8073d7b12790b6a4c26d
zom <jonathonwhite5@gmail.com>
2018-08-20 21:40:49 -0400

    added atomic unit conversions

--- frogcrab_3.py
+++ frogcrab_3.py
@@ -1,21 +1,95 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import constants as sc
+
+# SI units for defining parameters
+W = 1
+cm = 1e-2
+um = 1e-6
+fs = 1e-15
+atts = 1e-18
+
+class XUV_Field():
+
+    def __init__(self, N):
+
+        # define parameters in SI units
+        self.en0 = 150 * sc.eV # central energy
+        self.den0 = 75 * sc.eV #energy fwhm
+        self.f0 = self.en0/sc.h # carrier frequency
+        self.T0 = 1/self.f0 # optical cycle
+        self.t0 = 2 * sc.h * np.log(2) / (np.pi * self.den0) # pulse duration
+        self.gdd = 1000 * atts**2 # gdd
+
+        #discretize
+        self.tmax = 30 * self.t0
+        self.dt = self.tmax / N
+        self.tmat = self.dt * np.arange(-N/2, N/2, 1)
+
+        # discretize the streaking xuv field spectral matrix
+        self.df = 1/(self.dt * N)
+        self.fmat = self.df * np.arange(-N/2, N/2, 1)
+        self.enmat = sc.h * self.fmat
+
+        # convert to AU
+        self.en0 = self.en0 / sc.physical_constants['atomic unit of energy'][0]
+        self.den0 = self.den0 / sc.physical_constants['atomic unit of energy'][0]
+        self.t0 = self.t0 / sc.physical_constants['atomic unit of time'][0]
+        self.f0 = self.f0 * sc.physical_constants['atomic unit of time'][0]
+        self.T0 = self.T0 / sc.physical_constants['atomic unit of time'][0]
+        self.gdd = self.gdd / sc.physical_constants['atomic unit of time'][0]**2
+        self.dt = self.dt / sc.physical_constants['atomic unit of time'][0]
+        self.tmat = self.tmat / sc.physical_constants['atomic unit of time'][0]
+        self.fmat = self.fmat * sc.physical_constants['atomic unit of time'][0]
+        self.enmat = self.enmat / sc.physical_constants['atomic unit of energy'][0]
+
+        # set up streaking xuv field in AU
+        self.Et = np.exp(-2 * np.log(2) * (self.tmat/self.t0)**2 ) * np.exp(2j * np.pi * self.f0 * self.tmat)
+
+        # add GDD to streaking XUV field
+        self.Et = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(np.fft.fftshift(np.fft.fft(np.fft.fftshift(self.Et))) * (np.exp(0.5j * self.gdd * (2 * np.pi)**2) * self.fmat))))
 
 
-class Field():
+class IR_Field():
 
-    def __init__(self, N, w0, FWHM, tmax):
+    def __init__(self, N):
 
-        self.fwhm = FWHM
-        self.w0 = w0
+        # calculate parameters in SI units
+        self.lam0 = 1.7 * um    # central wavelength
+        self.f0 = sc.c/self.lam0    # carrier frequency
+        self.T0 = 1/self.f0 # optical cycle
+        self.t0 = 12 * fs # pulse duration
+        self.ncyc = self.t0/self.T0
+        self.I0 = 2e12 * W/cm**2
 
+        # compute ponderomotive energy
+        self.Up = (np.e**2 * self.I0) / (2 * sc.c * sc.epsilon_0 * sc.electron_mass * (2 * np.pi * self.f0)**2)
 
-        self.dt = (2*tmax) / N
-        self.t = self.dt * np.arange(-N/2, N/2, 1)
-        self.E_t = np.real(np.exp(1j * w0 * self.t) * np.exp(-2*np.log(4) * (self.t**2) / FWHM**2))
+        # discretize time matrix
+        self.tmax = 8 * self.t0
+        self.dt = self.tmax / N
+        self.tmat = self.dt * np.arange(-N/2, N/2, 1)
 
-        self.df = 1 / (N * self.dt)
-        self.F = self.df * np.arange(-N/2, N/2, 1)
+        # discretize spectral matrix
+        self.df = 1/(self.dt * N)
+        self.fmat = self.df * np.arange(-N/2, N/2, 1)
+        self.enmat = sc.h * self.fmat
+
+        # convert units to AU
+        self.t0 = self.t0 / sc.physical_constants['atomic unit of time'][0]
+        self.f0 = self.f0 * sc.physical_constants['atomic unit of time'][0]
+        self.T0 = self.T0 / sc.physical_constants['atomic unit of time'][0]
+        self.Up = self.Up / sc.physical_constants['atomic unit of energy'][0]
+        self.dt = self.dt / sc.physical_constants['atomic unit of time'][0]
+        self.tmat = self.tmat / sc.physical_constants['atomic unit of time'][0]
+        self.fmat = self.fmat * sc.physical_constants['atomic unit of time'][0]
+        self.enmat = self.enmat / sc.physical_constants['atomic unit of energy'][0]
+
+        # calculate driving amplitude in AU
+        self.E0 = np.sqrt(4 * self.Up * (2 * np.pi * self.f0)**2)
+
+        # set up the driving IR field amplitude in AU
+        self.Et = self.E0 * np.exp(-2 * np.log(2) * (self.tmat/self.t0)**2) * np.cos(2 * np.pi * self.f0 * self.tmat)
 
 
 def plot_t_cut(indexes, axis, products, number, product, axis_p, tau, p, S):
@@ -146,8 +220,12 @@ def attosecond_streak(xuv, ir, plot):
 
 
 
-xuv = Field(N=128, w0=8e17, FWHM=10e-18, tmax=50e-18)
-ir = Field(N=128, w0=1.5e14, FWHM=70e-15, tmax=100e-15)
+xuv = XUV_Field(N=2**8)
+# ir = IR_Field(N=128)
+
+exit(0)
+
+
 ir.E_t = ir.E_t
 
 
