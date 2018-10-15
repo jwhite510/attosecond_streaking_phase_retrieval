cb565218672a41d5a773fbf1021480d61e10df20
zom <jonathonwhite5@gmail.com>
2018-10-15 09:47:05 -0400

    added test and train split

--- generate_data.py
+++ generate_data.py
@@ -40,6 +40,7 @@ class XUV_Field_rand_phase(XUV_Field):
         self.Et_cropped_t_phase = np.fft.fftshift((np.fft.ifft(np.fft.fftshift(self.Et_cropped_f_phase))))
 
         # ambiguity removal
+
         # set the phase angle at t0 to 0
         Envelope = self.Et_cropped_t_phase * np.exp(-2j * np.pi * self.f0 * self.tmat_cropped)
         t_0_index = np.argmin(np.abs(self.tmat_cropped - 0))
@@ -47,41 +48,11 @@ class XUV_Field_rand_phase(XUV_Field):
         angle_at_t0 = np.angle(Envelope[t_0_index])
         Envelope_corrected = Envelope * np.exp(-1j * angle_at_t0)
 
-        # print('np.real(Envelope[t_0_index]):', np.real(Envelope[t_0_index]))
-        # print('np.imag(Envelope[t_0_index]):', np.imag(Envelope[t_0_index]))
-        #
-        # print('np.real(Envelope_corrected[t_0_index]):', np.real(Envelope_corrected[t_0_index]))
-        # print('np.imag(Envelope_corrected[t_0_index]):', np.imag(Envelope_corrected[t_0_index]))
-
         Corrected_centralfreq = Envelope_corrected * np.exp(2j * np.pi * self.f0 * self.tmat_cropped)
 
         self.Et_cropped_t_phase = Corrected_centralfreq
 
         # plt.plot(np.real(Corrected_centralfreq))
-        # plt.ioff()
-        # plt.show()
-
-
-        # Corrected_centralfreq2 = Corrected_centralfreq * np.exp(-2j * np.pi * self.f0 * self.tmat_cropped)
-
-        # for checking angle at time 0
-        # fig, ax = plt.subplots(5, 1)
-        #
-        # ax[0].plot(self.tmat_cropped, np.real(self.Et_cropped_t_phase), color='blue')
-        # ax[0].plot(self.tmat_cropped, np.imag(self.Et_cropped_t_phase), color='red')
-        #
-        # ax[1].plot(self.tmat_cropped, np.real(Envelope), color='blue')
-        # ax[1].plot(self.tmat_cropped, np.imag(Envelope), color='red')
-        #
-        # ax[2].plot(self.tmat_cropped, np.real(Envelope_corrected), color='blue')
-        # ax[2].plot(self.tmat_cropped, np.imag(Envelope_corrected), color='red')
-        #
-        # ax[3].plot(self.tmat_cropped, np.real(Corrected_centralfreq), color='blue')
-        # ax[3].plot(self.tmat_cropped, np.imag(Corrected_centralfreq), color='red')
-        #
-        # ax[4].plot(self.tmat_cropped, np.real(Corrected_centralfreq2), color='blue')
-        # ax[4].plot(self.tmat_cropped, np.imag(Corrected_centralfreq2), color='red')
-        #
         # plt.ioff()
         # plt.show()
 
@@ -110,39 +81,30 @@ class XUV_Field_rand_phase(XUV_Field):
             plt.show()
 
 
+def generate_samples(n_samples, filename):
 
 
-xuv_test = XUV_Field(N=N, tmax=tmax, gdd=500, tod=0).Et[lower:upper]
+    # create hdf5 file
+    print('creating file: '+filename)
+    hdf5_file = tables.open_file(filename, mode='w')
+    frog_image_f = hdf5_file.create_earray(hdf5_file.root,
+                                           'trace', tables.Float16Atom(), shape=(0, len(p_vec) * len(tauvec)))
+    E_real_f = hdf5_file.create_earray(hdf5_file.root,
+                                       'xuv_real', tables.Float16Atom(), shape=(0, len(xuv_test)))
+    E_imag_f = hdf5_file.create_earray(hdf5_file.root,
+                                       'xuv_imag', tables.Float16Atom(), shape=(0, len(xuv_test)))
+    hdf5_file.close()
 
-
-# create hdf5 file
-hdf5_file = tables.open_file('attstrace.hdf5', mode='w')
-frog_image_f = hdf5_file.create_earray(hdf5_file.root,
-                                       'trace', tables.Float16Atom(), shape=(0, len(p_vec) * len(tauvec)))
-E_real_f = hdf5_file.create_earray(hdf5_file.root,
-                                   'xuv_real', tables.Float16Atom(), shape=(0, len(xuv_test)))
-E_imag_f = hdf5_file.create_earray(hdf5_file.root,
-                                   'xuv_imag', tables.Float16Atom(), shape=(0, len(xuv_test)))
-hdf5_file.close()
-
-fig, ax = plt.subplots(2, 1)
-n_samples = 20000
-plt.ion()
-plotting = True
-init = tf.global_variables_initializer()
-with tf.Session() as sess:
-
-    init.run()
 
     # open the hdf5 file
-    hdf5_file = tables.open_file('attstrace.hdf5', mode='a')
+    hdf5_file = tables.open_file(filename, mode='a')
 
     for i in range(n_samples):
 
         # generate a random xuv pulse
         xuv_rand = XUV_Field_rand_phase(phase_amplitude=5, phase_nodes=120, plot=False).Et_cropped_t_phase
 
-        if i % 1000 == 0:
+        if i % 500 == 0:
             print('generating sample {} of {}'.format(i + 1, n_samples))
             # generate the FROG trace
             time1 = time.time()
@@ -162,9 +124,6 @@ with tf.Session() as sess:
         else:
             strace = sess.run(image, feed_dict={xuv_input: xuv_rand.reshape(1, -1, 1)})
 
-
-
-
         # divide the xuv into real and imaginary
         xuv_real = np.real(xuv_rand)
         xuv_imag = np.imag(xuv_rand)
@@ -174,20 +133,39 @@ with tf.Session() as sess:
         hdf5_file.root.xuv_imag.append(xuv_imag.reshape(1, -1))
         hdf5_file.root.trace.append(strace.reshape(1, -1))
 
-
-
-
     hdf5_file.close()
 
 
-# test open the file
-hdf5_file = tables.open_file('attstrace.hdf5', mode='r')
 
-index = 0
+xuv_test = XUV_Field(N=N, tmax=tmax, gdd=500, tod=0).Et[lower:upper]
+
+
+
+
+fig, ax = plt.subplots(2, 1)
+# n_train_samples = 20000
+# n_test_samples = 1000
+
+plt.ion()
+plotting = True
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+
+    n_train_samples = 20000
+    n_test_samples = 1000
+
+    init.run()
+
+    generate_samples(n_samples=n_train_samples, filename='attstrace_train.hdf5')
+    generate_samples(n_samples=n_test_samples, filename='attstrace_test.hdf5')
+
+
+# test open the file
+index = 20
+hdf5_file = tables.open_file('attstrace_train.hdf5', mode='r')
+# hdf5_file = tables.open_file('attstrace_test.hdf5', mode='r')
 
 xuv = hdf5_file.root.xuv_real[index, :] + 1j * hdf5_file.root.xuv_imag[index, :]
-
-
 plt.ioff()
 plt.cla()
 ax[0].pcolormesh(hdf5_file.root.trace[index, :].reshape(len(p_vec), len(tauvec)), cmap='jet')
@@ -195,7 +173,6 @@ ax[0].text(0.2, 0.9, 'index: {}'.format(str(index)), transform=ax[0].transAxes, 
 ax[1].plot(np.abs(xuv), color='black', linestyle='dashed', alpha=0.5)
 ax[1].plot(np.real(xuv), color='blue')
 ax[1].plot(np.imag(xuv), color='red')
-
 hdf5_file.close()
 
 plt.show()
