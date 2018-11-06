3dab29d10a89e759f87d4b6d40998ab83eb97c89
zom <jonathonwhite5@gmail.com>
2018-11-06 15:10:52 -0500

    mostly finished making the plots look nice

--- retrieval.py
+++ retrieval.py
@@ -9,6 +9,73 @@ modelname = 'reg_conv_net_11_5_18_linmomentum'
 from models.network_reg_conv_net_11_5_18_linmomentum import *
 import tensorflow as tf
 import scipy.constants as sc
+import pickle
+
+
+def plot_single_trace(x_in, y_in):
+
+    # initialize the plot for single trace
+    fig = plt.figure(figsize=(10,7))
+    borderspace_lr = 0.1
+    borderspace_tb = 0.1
+
+    plt.subplots_adjust(left=0+borderspace_lr, right=1-borderspace_lr,
+                        top=1-borderspace_tb, bottom=0+borderspace_tb,
+                        hspace=0.3, wspace=0.4)
+    gs = fig.add_gridspec(2, 2)
+
+    # generate time for plotting
+    si_time = tauvec * dt * sc.physical_constants['atomic unit of time'][0]
+
+    predictions = sess.run(y_pred, feed_dict={x: x_in, y_true: y_in})
+    index = 0
+    mse = sess.run(loss, feed_dict={x: x_in[index].reshape(1, -1), y_true: y_in[index].reshape(1, -1)})
+
+    # determine phase crop
+    crop_phase_right = 15
+    crop_phase_left = 20
+
+    # plot input trace
+    axis = fig.add_subplot(gs[0,:])
+    axis.pcolormesh(si_time*1e15, p_vec, x_in[index].reshape(len(generate_proof_traces.p_vec), len(generate_proof_traces.tauvec)), cmap='jet')
+    axis.set_xlabel('Delay [Femtoseconds]')
+    axis.set_ylabel('Momentum [a.u]')
+    axis.text(0.3, 1.05, "Input Streaking Trace", transform=axis.transAxes, backgroundcolor='white', weight='bold')
+
+
+
+    # plot the predicted spectral phase
+    axis = fig.add_subplot(gs[1,1])
+    axis.cla()
+    complex_field = predictions[index, :64] + 1j * predictions[index, 64:]
+    axis.plot(electronvolts, np.abs(complex_field), color="black")
+    axtwin = axis.twinx()
+    # plot the phase
+    axtwin.plot(electronvolts[crop_phase_left:-crop_phase_right],np.unwrap(np.angle(complex_field[crop_phase_left:-crop_phase_right])), color="green")
+    axtwin.set_ylabel(r"$\phi(eV)$")
+    axtwin.yaxis.label.set_color('green')
+    axtwin.tick_params(axis='y', colors='green')
+    # plot the error
+    axis.text(0.5, 1.05, "MSE: " + str(round(mse, 5)),transform=axis.transAxes, backgroundcolor='white')
+    axis.text(0, 1.05, "Prediction", transform=axis.transAxes, backgroundcolor='white', weight='bold')
+    axis.set_xlabel('Energy [eV]')
+
+
+    # plot the actual spectral phase
+    axis = fig.add_subplot(gs[1, 0])
+    axis.cla()
+    complex_field = y_in[index, :64] + 1j * y_in[index, 64:]
+    axis.plot(electronvolts, np.abs(complex_field), color="black")
+    axtwin = axis.twinx()
+    # plot the phase
+    axtwin.plot(electronvolts[crop_phase_left:-crop_phase_right],np.unwrap(np.angle(complex_field[crop_phase_left:-crop_phase_right])), color="green")
+    axtwin.set_ylabel(r"$\phi(eV)$")
+    axtwin.yaxis.label.set_color('green')
+    axtwin.tick_params(axis='y', colors='green')
+    axis.text(0, 1.05, "Actual", transform=axis.transAxes, backgroundcolor='white', weight='bold')
+    axis.set_xlabel('Energy [eV]')
+
+
 
 
 
@@ -126,9 +193,17 @@ fmat_hz = fmat / sc.physical_constants['atomic unit of time'][0]
 fmat_joules = sc.h * fmat_hz # joules
 electronvolts = 1 / (sc.elementary_charge) * fmat_joules
 
+# get momentum vector for plotting
+with open('crab_tf_items.p', 'rb') as file:
+    crab_tf_items = pickle.load(file)
+    p_vec = crab_tf_items['p_vec']
+    dt = crab_tf_items['dt']
+    tauvec = crab_tf_items['tauvec']
 
 
-#initialize the plot
+
+
+#initialize the plot for multiple traces
 fig2, ax2 = plt.subplots(3, 3, figsize=(14, 8))
 plt.subplots_adjust(left=0.05, right=0.95, top=0.92, bottom=0.05,
                         wspace=0.1, hspace=0.1)
@@ -144,6 +219,8 @@ with tf.Session() as sess:
     batch_x_test, batch_y_test = get_data.evaluate_on_test_data()
     plot_predictions(x_in=batch_x_test, y_in=batch_y_test, axis=ax2, fig=fig2,
                      set="test", modelname=modelname, epoch=0)
+
+    plot_single_trace(x_in=batch_x_test, y_in=batch_y_test)
 
     plt.ioff()
     plt.show()
