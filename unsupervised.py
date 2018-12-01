217af6627c1180d1828b0630da9ded61ed50339d
zom <jonathonwhite5@gmail.com>
2018-11-30 19:27:10 -0500

    added tensorboard plotting and showing mse on graph

--- unsupervised.py
+++ unsupervised.py
@@ -30,6 +30,11 @@ def update_plots(generated_image, input_image, actual_fields, predicted_fields):
 
     iteration = i+1
 
+    # calculate loss
+    loss_value = sess.run(network2.u_losses, feed_dict={network2.x: trace})
+    print('iteration: {}'.format(iteration))
+    print('loss_value: ', loss_value)
+
     axes['input_ir'].cla()
     axes['input_ir'].plot(np.real(actual_fields['ir_f']), color='blue')
     axes['input_ir'].plot(np.imag(actual_fields['ir_f']), color='red')
@@ -54,6 +59,8 @@ def update_plots(generated_image, input_image, actual_fields, predicted_fields):
     axes['generated_image'].cla()
     axes['generated_image'].pcolormesh(generated_image)
     axes['generated_image'].text(0.0, 1.0, 'iteration: {}'.format(iteration), transform = axes['generated_image'].transAxes, backgroundcolor='white')
+    axes['generated_image'].text(0.0, 0.0, 'loss: {}'.format(loss_value),
+                                 transform=axes['generated_image'].transAxes, backgroundcolor='white')
 
     plt.pause(0.001)
 
@@ -79,6 +86,12 @@ def get_trace(index, filename):
 
 
 
+def add_tensorboard_values():
+
+    summ = sess.run(unsupervised_mse_tb, feed_dict={network2.x: trace})
+    writer.add_summary(summ, global_step=i + 1)
+
+
 # copy the model to a new version to use for unsupervised learning
 modelname = '2_test'
 for file in glob.glob(r'./models/{}.ckpt.*'.format(modelname)):
@@ -89,11 +102,16 @@ for file in glob.glob(r'./models/{}.ckpt.*'.format(modelname)):
 # get the trace
 trace, actual_fields = get_trace(index=0, filename='attstrace_test2_processed.hdf5')
 
+# create mse tb measurer
+unsupervised_mse_tb = tf.summary.scalar("test_mse", network2.u_losses)
+
 
 axes = create_plots()
 
 plt.ion()
 with tf.Session() as sess:
+
+    writer = tf.summary.FileWriter("./tensorboard_graph_u/" + modelname)
 
     saver = tf.train.Saver()
     saver.restore(sess, './models/{}.ckpt'.format(modelname+'_unsupervised'))
@@ -115,25 +133,11 @@ with tf.Session() as sess:
         # plot the trace and generated image
         update_plots(generated_image, trace_2d, actual_fields, predicted_fields)
 
+        # add tensorbaord values
+        add_tensorboard_values()
+
         # train the network to reduce the error
         sess.run(network2.u_train, feed_dict={network2.x: trace})
-
-
-
-
-
-
-    # retrieve output from network
-    # ir_xuv_output = sess.run(network2.y_pred, feed_dict={network2.x: trace})
-
-    # plot streaking trace of the output
-    # xuv_pred, ir_pred = network2.separate_xuv_ir_vec(ir_xuv_output[0])
-    # generated_trace = sess.run(crab_tf2.image, feed_dict={crab_tf2.ir_cropped_f: ir_pred,
-    #                                                       crab_tf2.xuv_cropped_f: xuv_pred})
-
-
-
-
 
 
 
