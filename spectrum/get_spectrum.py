18d3a2cbe7ac8af8dda29fcc396244426743b44b
jonathon <jonathonwhite5@gmail.com>
2018-12-14 12:37:03 -0500

    fixed to output field amplitude instead of intensity

--- spectrum/get_spectrum.py
+++ spectrum/get_spectrum.py
@@ -7,85 +7,83 @@ import pickle
 
 with open('xuv_spectrum.csv', 'r') as file:
     reader = csv.reader(file)
-    x = []
-    y = []
+    electronvolts = []
+    Intensity = []
     for row in reader:
-        x.append(float(row[0]))
-        y.append(float(row[1]))
+        electronvolts.append(float(row[0]))
+        Intensity.append(float(row[1]))
 
 
-
-# define fmat and tmat
-N = 512
 
 # convert eV to joules
-joules = np.array(x) * sc.electron_volt # joules
-hertz = joules / sc.h
+joules = np.array(electronvolts) * sc.electron_volt # joules
+hertz = np.array(joules / sc.h)
+Intensity = np.array(Intensity)
 
-hertz = np.array(hertz)
-y = np.array(y)
-
-
+# define tmat and famt
+N = 512
 tmax = 400e-18
 dt = 2 * tmax / N
 tmat = dt * np.arange(-N/2, N/2, 1)
 df = 1 / (N * dt)
 fmat = df * np.arange(-N/2, N/2, 1)
 
-plt.figure(1)
-plt.plot(hertz, y, color='black')
 
-# append points
-# print(hertz)
+# pad the retrieved values with zeros to interpolate later
 hertz = np.insert(hertz, 0, -6e17)
-y = np.insert(y, 0, 0)
+Intensity = np.insert(Intensity, 0, 0)
 hertz = np.insert(hertz, -1, 6e17)
-y = np.insert(y, -1, 0)
-y[y<0] = 0
-f0 = hertz[np.argmax(y)]
+Intensity = np.insert(Intensity, -1, 0)
+Intensity[Intensity<0] = 0
+
+# get the carrier frequency
+f0 = hertz[np.argmax(Intensity)]
 
 
-
-plt.plot(hertz, y, color='red')
-plt.plot(fmat, np.zeros_like(fmat), color='blue')
-
+# square root the intensity to get electric field amplitude
+Ef = np.sqrt(Intensity)
 
 # map the spectrum onto 512 points linearly
-interpolator = scipy.interpolate.interp1d(hertz, y, kind='linear')
-Ef = interpolator(fmat)
+interpolator = scipy.interpolate.interp1d(hertz, Ef, kind='linear')
+Ef_interp = interpolator(fmat)
 
 
+# calculate signal in time
+linear_E_t = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(Ef_interp)))
 
-
-
-plt.figure(2)
-plt.plot(fmat, Ef)
-plt.xlabel('frequency [Hz]')
-plt.ylabel('Amplitude')
-# plt.plot(Ef)
-
-linear_E_t = np.fft.fftshift(np.fft.ifft(np.fft.fftshift(Ef)))
-
-plt.figure(3)
-plt.plot(tmat, np.real(linear_E_t))
-# plt.plot(np.real(linear_E_t))
-
-
-# crop the xuv field
-plt.figure(4)
-plt.plot(fmat, Ef, color='red', alpha=0.5)
+# set the indexes for cropped input
 indexmin = 270
 indexmax = 325
 
-plt.plot(fmat[indexmin:indexmax], Ef[indexmin:indexmax], color='red')
+
+
+plt.figure(1)
+plt.plot(hertz, Intensity, color='red')
+plt.plot(fmat, np.zeros_like(fmat), color='blue')
+
+
+plt.figure(2)
+plt.plot(fmat, Ef_interp, label='|E|')
+plt.plot(hertz, Intensity, label='I')
+plt.xlabel('frequency [Hz]')
+plt.legend(loc=1)
+
+
+plt.figure(3)
+plt.plot(tmat, np.real(linear_E_t))
+
+
+plt.figure(4)
+plt.plot(fmat, Ef_interp, color='red', alpha=0.5)
+plt.plot(fmat[indexmin:indexmax], Ef_interp[indexmin:indexmax], color='red')
 
 
 
-
+# pickle the files for use in xuv pulse generation
 pickle_files = {}
 pickle_files['tmat'] = tmat
 pickle_files['fmat'] = fmat
-pickle_files['Ef'] = Ef
+pickle_files['Ef'] = Ef_interp
 pickle_files['indexmin'] = indexmin
 pickle_files['indexmax'] = indexmax
 pickle_files['f0'] = f0
