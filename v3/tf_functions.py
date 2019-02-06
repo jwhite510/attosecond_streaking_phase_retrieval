d603c243383118b6af44e6f0e559e6120713a120
zom <jonathonwhite5@gmail.com>
2019-02-05 19:58:20 -0500

    begin v3

---
+++ v3/tf_functions.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+# from xuv_spectrum import spectrum
+
+import xuv_spectrum.spectrum
+
+exit(0)
+import spectrum
+
+
+
+# get the spectrum of the XUV
+
+
+# def construct_xuv(xuv_phase, xuv_spec):
+#
---
+++ v3/xuv_spectrum/spectrum.py
@@ -0,0 +1,1 @@
+print("get the spectrum")
