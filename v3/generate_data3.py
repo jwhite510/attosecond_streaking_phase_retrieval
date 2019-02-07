af7fe0c158dedfba56072d06a047652183a89533
zom <jonathonwhite5@gmail.com>
2019-02-07 18:20:59 -0500

    working

--- v3/generate_data3.py
+++ v3/generate_data3.py
@@ -5,7 +5,51 @@ import numpy as np
 import scipy.constants as sc
 import matplotlib.pyplot as plt
 import xuv_spectrum.spectrum
+import ir_spectrum.ir_spectrum
+import time
 
+
+def plot_opened_file(xuv_coefs, ir_params, trace, sess, tf_graphs, streak_params):
+
+    fig = plt.figure()
+    gs = fig.add_gridspec(2, 2)
+
+    print("xuv_coefs: ", xuv_coefs)
+    print("ir_params: ", ir_params)
+
+    xuv_t = sess.run(tf_graphs["xuv_E_prop"]["t"], feed_dict={tf_graphs["xuv_coefs_in"]: xuv_coefs.reshape(1, -1)})
+
+    ir_t = sess.run(tf_graphs["ir_E_prop"]["t"], feed_dict={tf_graphs["ir_values_in"]: ir_params.reshape(1, -1)})
+
+    # plot xuv
+    ax = fig.add_subplot(gs[0, 0])
+    ax.plot(xuv_spectrum.spectrum.tmat, np.real(xuv_t[0]), color='blue')
+
+    # plot ir
+    ax = fig.add_subplot(gs[0, 1])
+    ax.plot(ir_spectrum.ir_spectrum.tmat, np.real(ir_t[0]), color='blue')
+
+
+    # plot trace
+    ax = fig.add_subplot(gs[1, :])
+    ax.pcolormesh(streak_params["tau_values"], streak_params["p_values"],
+                  trace.reshape(len(streak_params["p_values"]), len(streak_params["tau_values"])), cmap='jet')
+
+
+def update_plots2(axes, trace, xuv_t, threshold):
+
+    axes[0].cla()
+    axes[0].pcolormesh(trace, cmap='jet')
+    axes[1].cla()
+    #    ax[1].plot(xuv.tmat, np.real(xuv.Et_prop), color='blue')
+    axes[1].plot(np.real(xuv_t), color='blue')
+    maxval = np.max(np.abs(xuv_t))
+
+    axes[1].plot([threshold["indexes"][0], threshold["indexes"][0]], [maxval, -maxval], color='black')
+    axes[1].plot([threshold["indexes"][1], threshold["indexes"][1]], [maxval, -maxval], color='black')
+    axes[1].plot([threshold["indexes"][0], threshold["indexes"][1]], [threshold["threshold"], threshold["threshold"]], color='red', linestyle='dashed')
+
+    plt.pause(0.001)
 
 
 def check_time_boundary(indexmin, indexmax, threshold, xuv_t, bad_samples):
@@ -19,31 +63,30 @@ def check_time_boundary(indexmin, indexmax, threshold, xuv_t, bad_samples):
     if value_1 > threshold or value_2 > threshold:
     # if True:
         bad_samples+=1
-        plt.figure(356)
-        plt.cla()
-        plt.plot(np.real(xuv_t))
-        plt.plot([indexmin, indexmin], [-np.max(np.real(xuv_t)), np.max(np.real(xuv_t))], color='black')
-        plt.plot([indexmax, indexmax], [-np.max(np.real(xuv_t)), np.max(np.real(xuv_t))], color='black')
-        plt.plot([indexmin, indexmax], [threshold, threshold], color='red', linestyle='dashed')
-        plt.text(0.1, 0.8, 'bad samples:{}'.format(bad_samples), transform=plt.gca().transAxes)
-        plt.show()
-        plt.pause(0.001)
+        # plt.figure(356)
+        # plt.cla()
+        # plt.plot(np.real(xuv_t))
+        # plt.plot([indexmin, indexmin], [-np.max(np.real(xuv_t)), np.max(np.real(xuv_t))], color='black')
+        # plt.plot([indexmax, indexmax], [-np.max(np.real(xuv_t)), np.max(np.real(xuv_t))], color='black')
+        # plt.plot([indexmin, indexmax], [threshold, threshold], color='red', linestyle='dashed')
+        # plt.text(0.1, 0.8, 'bad samples:{}'.format(bad_samples), transform=plt.gca().transAxes)
+        # plt.show()
+        # plt.pause(0.001)
         return bad_samples, False
 
     return bad_samples, True
 
 
-
-
-
-
-def generate_samples(tf_graphs, n_samples, filename, streak_params, xuv_coefs, sess):
+def generate_samples(tf_graphs, n_samples, filename, streak_params, xuv_coefs, sess, axis):
     print('creating file: ' + filename)
 
     # create hdf5 file
     with tables.open_file(filename, mode='w') as hd5file:
         # create array for trace
         hd5file.create_earray(hd5file.root, 'trace', tables.Float64Atom(), shape=(0, len(streak_params["p_values"]) * len(streak_params["tau_values"])))
+
+        # noise trace
+        hd5file.create_earray(hd5file.root, 'noise_trace', tables.Float64Atom(),shape=(0, len(streak_params["p_values"]) * len(streak_params["tau_values"])))
 
         # create array for XUV
         hd5file.create_earray(hd5file.root, 'xuv_coefs', tables.Float64Atom(), shape=(0, xuv_coefs))
@@ -57,7 +100,13 @@ def generate_samples(tf_graphs, n_samples, filename, streak_params, xuv_coefs, s
     # make a sample with no phase to give a comparison
     xuv_coefs_in = np.array([[0.0, 0.0, 0.0, 0.0, 0.0]])
     xuv_t = sess.run(tf_graphs["xuv_E_prop"]["t"], feed_dict={tf_graphs["xuv_coefs_in"]: xuv_coefs_in})
-    threshold = np.max(np.abs(xuv_t)) / 200
+    threshold = np.max(np.abs(xuv_t)) / 300
+    indexmin = 100
+    indexmax = 924
+
+    threshold_dict = {}
+    threshold_dict["threshold"] = threshold
+    threshold_dict["indexes"] = (indexmin, indexmax)
 
     # count the number of bad samples generated
     bad_samples = 0
@@ -68,11 +117,12 @@ def generate_samples(tf_graphs, n_samples, filename, streak_params, xuv_coefs, s
         for i in range(n_samples):
 
             xuv_good = False
-            indexmin = 100
-            indexmax = 924
+
             while not xuv_good:
 
                 # xuv_coefs_in = np.array([[0.0, 0.0, 0.0, 0.0, 0.0]])
+
+                #random coefficients between -0.5 and 0.5
                 xuv_coefs_in = (np.random.rand(5)-0.5).reshape(1, -1)
 
                 # generate time pulse from these coefficients
@@ -81,23 +131,41 @@ def generate_samples(tf_graphs, n_samples, filename, streak_params, xuv_coefs, s
                 # check if the pulse is constrained in time window
                 bad_samples, xuv_good = check_time_boundary(indexmin, indexmax, threshold, xuv_t[0], bad_samples)
 
-            print("xuv was good")
 
-            xuv_t = sess.run(tf_graphs["xuv_E_prop"]["t"], feed_dict={tf_graphs["xuv_coefs_in"]: xuv_coefs_in})
-            plt.figure(111)
-            plt.plot(xuv_spectrum.spectrum.tmat, np.real(xuv_t[0]))
-            plt.show()
+            # make ir params
+            ir_values_in = (2.0*np.random.rand(4)-1.0).reshape(1, -1)
 
-            exit(0)
+            # generate streaking trace
+            if i % 500 == 0:
+                print('generating sample {} of {}'.format(i + 1, n_samples))
+                print('bad sample count: {}'.format(bad_samples))
+                # generate the streaking trace
+                time1 = time.time()
+
+                trace = sess.run(tf_graphs["image"], feed_dict={tf_graphs["xuv_coefs_in"]: xuv_coefs_in,
+                                                                tf_graphs["ir_values_in"]: ir_values_in})
+
+                update_plots2(axes=axis, trace=trace, xuv_t=xuv_t[0], threshold=threshold_dict)
 
 
+                time2 = time.time()
+                duration = time2 - time1
+                print('duration: {} s'.format(round(duration, 4)))
+
+            else:
+                trace = sess.run(tf_graphs["image"], feed_dict={tf_graphs["xuv_coefs_in"]: xuv_coefs_in,
+                                                                tf_graphs["ir_values_in"]: ir_values_in})
 
 
+            # add noise to trace
+            noise_trace = add_shot_noise(trace)
 
 
-
-
-
+            # append data sample
+            hd5file.root.trace.append(trace.reshape(1, -1))
+            hd5file.root.noise_trace.append(noise_trace.reshape(1, -1))
+            hd5file.root.xuv_coefs.append(xuv_coefs_in.reshape(1, -1))
+            hd5file.root.ir_params.append(ir_values_in.reshape(1, -1))
 
 
 def add_shot_noise(trace_sample):
@@ -120,15 +188,12 @@ def add_shot_noise(trace_sample):
 
 
 
-
-
-
 if __name__ == "__main__":
 
     # initialize XUV generator
     xuv_phase_coeffs = 5
     xuv_coefs_in = tf.placeholder(tf.float32, shape=[None, xuv_phase_coeffs])
-    xuv_E_prop = tf_functions.xuv_taylor_to_E(xuv_coefs_in, amplitude=9.0)
+    xuv_E_prop = tf_functions.xuv_taylor_to_E(xuv_coefs_in, amplitude=12.0)
 
     # initialize IR generator
     # IR amplitudes
@@ -159,26 +224,33 @@ if __name__ == "__main__":
     tf_graphs["ir_E_prop"] = ir_E_prop
     tf_graphs["image"] = image
 
+    # create plot to show samples as they are generated
+    _, ax = plt.subplots(2, 1, figsize=(5, 5))
     with tf.Session() as sess:
 
 
-        generate_samples(tf_graphs=tf_graphs, n_samples=1000,
+        generate_samples(tf_graphs=tf_graphs, n_samples=60000,
                          filename="train3.hdf5", streak_params=streak_params,
-                         xuv_coefs=xuv_phase_coeffs, sess=sess)
+                         xuv_coefs=xuv_phase_coeffs, sess=sess, axis=ax)
+
+        generate_samples(tf_graphs=tf_graphs, n_samples=500,
+                         filename="test3.hdf5", streak_params=streak_params,
+                         xuv_coefs=xuv_phase_coeffs, sess=sess, axis=ax)
 
 
+        # test open the file
+        index = 2
+        with tables.open_file('train3.hdf5', mode='r') as hd5file:
+            xuv_coefs = hd5file.root.xuv_coefs[index, :]
+            ir_params = hd5file.root.ir_params[index, :]
+            trace = hd5file.root.noise_trace[index, :]
 
+        plot_opened_file(xuv_coefs=xuv_coefs, ir_params=ir_params,
+                         trace=trace, sess=sess, tf_graphs=tf_graphs,
+                         streak_params=streak_params)
 
-
-    with tf.Session() as sess:
-        # between -0.5 and 0.5
-        xuv_coefs = np.array([[0.0, 0.0, 0.0, 0.0, 0.0]])
-        # between -1 and 1
-        ir_values = np.array([[0.0, 0.0, 0.0, 0.0]])
-        image_sample = sess.run(image, feed_dict={xuv_coefs_in: xuv_coefs,
-                                               ir_values_in: ir_values})
-
-        image_sample = add_shot_noise(image_sample)
+        plt.ioff()
+        plt.show()
 
 
 
