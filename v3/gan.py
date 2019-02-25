ba62b20c84b2abb054e89a3573994b9ea065a910
jonathon <jonathonwhite5@gmail.com>
2019-02-25 17:54:10 -0500

    training with tracking

--- v3/gan.py
+++ v3/gan.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     # run name
     # can do multiple run names for the same model
-    run_name = 'gan_test_1'
+    run_name = 'gan_test_20'
 
 
     # copy the model to a new version to use for unsupervised learning
@@ -31,26 +31,37 @@ if __name__ == "__main__":
         file_newname = file.replace(modelname, modelname+'_gan')
         shutil.copy(file, file_newname)
 
+    writer = tf.summary.FileWriter("./tensorboard_graph_gan/" + modelname+"_gan_"+run_name)
+
 
     tf_generator_graphs, streak_params = network3.initialize_xuv_ir_trace_graphs()
     nn_nodes = network3.setup_neural_net(streak_params)
+
+    gan_mse_tb = tf.summary.scalar("gan_mse", nn_nodes["gan"]["gan_network_loss"])
 
 
     with tf.Session() as sess:
         saver = tf.train.Saver()
         saver.restore(sess, "./models/{}.ckpt".format(modelname + "_gan"))
 
-        traces, labels = get_data.evaluate_on_test_data()
+        # train gan network
+        for i in range(20000):
 
-        out = sess.run(nn_nodes["reconstruction"]["trace"], feed_dict={nn_nodes["trace_in"]: traces[0].reshape(1, -1)})
+            #print("training")
 
-        #plt.figure(1)
-        #plt.pcolormesh(out)
+            gan_input = np.random.rand(100).reshape(1, -1)
+            sess.run(nn_nodes["gan"]["train"], feed_dict={nn_nodes["gan"]["learningrate"]: 0.0001,
+                                                          nn_nodes["gan"]["gan_input"]: gan_input
+                                                          })
 
-        #plt.figure(2)
-        #plt.pcolormesh(traces[0].reshape(301, 58))
+            if i % 10 == 0:
+                summ = sess.run(gan_mse_tb, feed_dict={nn_nodes["gan"]["gan_input"]: gan_input})
+                writer.add_summary(summ, global_step=i + 1)
+                writer.flush()
 
-        #plt.show()
+
+        #traces, labels = get_data.evaluate_on_test_data()
+        #out = sess.run(nn_nodes["reconstruction"]["trace"], feed_dict={nn_nodes["trace_in"]: traces[0].reshape(1, -1)})
 
         gan_input = np.random.rand(100).reshape(1, -1)
 
@@ -72,23 +83,7 @@ if __name__ == "__main__":
         plt.plot(np.real(out[0]), color="blue")
         plt.plot(np.imag(out[0]), color="red")
 
-
-
-
-
         plt.show()
 
 
-
-
-
-
-
         exit(0)
-
-
-
-
-
-
-
