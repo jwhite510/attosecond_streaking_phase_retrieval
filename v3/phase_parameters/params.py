6129ed8f0992382abfe361e36cc13cad25d9034e
zom <jonathonwhite5@gmail.com>
2019-02-24 20:24:49 -0500

    switch to one wavelength ,and reduce phase amplitude

--- v3/phase_parameters/params.py
+++ v3/phase_parameters/params.py
@@ -5,14 +5,14 @@ import scipy.constants as sc
 # includes linear
 xuv_phase_coefs=5
 # phase amplitude
-amplitude=9.0
+amplitude=8.0
 # amplitude=4.0
 
 #infrared params
 ir_param_amplitudes = {}
 ir_param_amplitudes["phase_range"] = (0, 2 * np.pi)
-# ir_param_amplitudes["clambda_range"] = (1.6345, 1.6345)
-ir_param_amplitudes["clambda_range"] = (1.0, 1.6345)
+ir_param_amplitudes["clambda_range"] = (1.6345, 1.6345)
+# ir_param_amplitudes["clambda_range"] = (1.0, 1.6345)
 ir_param_amplitudes["pulseduration_range"] =  (7.0, 12.0)
 ir_param_amplitudes["I_range"] = (0.4, 1.0)
 
@@ -24,7 +24,7 @@ Ip = Ip / sc.physical_constants['atomic unit of energy'][0]  # a.u.
 
 
 # threshold scaler for the generated pulses
-threshold_scaler = 0.01
+threshold_scaler = 0.03
 
 # threshold_min_index = 100
 threshold_min_index = 50
