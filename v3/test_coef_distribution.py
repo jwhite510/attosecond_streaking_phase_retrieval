b6f46ab78e6d44e0374dd0792cd539be9b5c9fb3
zom <jonathonwhite5@gmail.com>
2019-02-27 18:31:49 -0500

    file for testing coefficient distributions in generated data set

---
+++ v3/test_coef_distribution.py
@@ -0,0 +1,74 @@
+import tables
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    phi_2 = []
+    phi_3 = []
+    phi_4 = []
+    phi_5 = []
+
+
+    with tables.open_file('train3.hdf5', mode='r') as hd5file:
+
+        # index = 0
+        samples = len(hd5file.root.xuv_coefs[:, :])
+
+        for index in range(samples):
+
+            xuv_coefs = hd5file.root.xuv_coefs[index, :]
+            phi_2.append(xuv_coefs[1])
+            phi_3.append(xuv_coefs[2])
+            phi_4.append(xuv_coefs[3])
+            phi_5.append(xuv_coefs[4])
+
+
+    plt.figure(1)
+    plt.hist(phi_2, color="blue", width=0.02)
+    plt.title("$\phi$ 2")
+    plt.xlabel("coef value")
+    plt.ylabel("count")
+
+    plt.figure(2)
+    plt.hist(phi_3, color="blue", width=0.02)
+    plt.title("$\phi$ 3")
+    plt.xlabel("coef value")
+    plt.ylabel("count")
+
+    plt.figure(3)
+    plt.hist(phi_4, color="blue", width=0.02)
+    plt.title("$\phi$ 4")
+    plt.xlabel("coef value")
+    plt.ylabel("count")
+
+    plt.figure(4)
+    plt.hist(phi_5, color="blue", width=0.02)
+    plt.title("$\phi$ 5")
+    plt.xlabel("coef value")
+    plt.ylabel("count")
+
+
+
+
+
+
+
+
+    plt.show()
+
+
+
+
+
+
+
