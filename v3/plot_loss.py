a2925c012c70da3bf8a701f6687d869ee8193ac7
jonathon <jonathonwhite5@gmail.com>
2019-03-20 19:31:36 -0400

    plot on one graph, and looks a lot nicer

--- v3/plot_loss.py
+++ v3/plot_loss.py
@@ -13,49 +13,42 @@ def get_csv(filename):
 
 
 
-
-
-
+fig, ax = plt.subplots(3, 1, figsize=(6,9))
+fig.subplots_adjust(wspace=0.0, hspace=0.0, top=1.0, left=0.1, bottom=0.1)
 data = get_csv(filename="run_test1_phasecurve-tag-train_mse_coef_params.csv")
-plt.figure(1)
-plt.plot(data[:, 1], data[:, 2], color="blue")
-plt.plot([15, 15], [0, np.max(data[:, 2])], color="red")
-plt.plot([30, 30], [0, np.max(data[:, 2])], color="red")
-plt.ylabel("MSE")
-plt.xlabel("Epoch")
-plt.gca().set_yscale("log")
-# plt.xlim(0, 60)
-plt.title("Coefficient Parameters MSE")
-plt.savefig("CoefficientParametersMSE_full.png")
+ax[0].plot(data[:, 1], data[:, 2], color="blue", label="Coefficient\nParameters MSE")
+ax[0].plot([15, 15], [0, np.max(data[:, 2])], color="red")
+ax[0].plot([30, 30], [0, np.max(data[:, 2])], color="red")
+ax[0].set_yscale("log")
+ax[0].set_ylim(0, np.max(data[:, 2]))
+ax[0].set_xticks([])
+# ax[0].set_xlim(0, 60)
+ax[0].legend()
 
 
 data = get_csv(filename="run_test1_phasecurve-tag-train_mse_fields.csv")
-plt.figure(2)
-plt.plot(data[:, 1], data[:, 2], color="blue")
-plt.plot([15, 15], [0, np.max(data[:, 2])], color="red")
-plt.plot([30, 30], [0, np.max(data[:, 2])], color="red")
-plt.ylabel("MSE")
-plt.xlabel("Epoch")
-plt.gca().set_yscale("log")
-# plt.xlim(0, 60)
-plt.title("Fields vector MSE")
-plt.savefig("FieldsvectorMSE_full.png")
+ax[1].plot(data[:, 1], data[:, 2], color="blue", label="Fields\nvector MSE")
+ax[1].plot([15, 15], [0, np.max(data[:, 2])], color="red")
+ax[1].plot([30, 30], [0, np.max(data[:, 2])], color="red")
+ax[1].set_yscale("log")
+ax[1].set_ylim(0, np.max(data[:, 2]))
+ax[1].set_xticks([])
+# ax[1].set_xlim(0, 60)
+ax[1].legend()
 
 
 data = get_csv(filename="run_test1_phasecurve-tag-train_mse_phasecurve.csv")
-plt.figure(3)
-plt.plot(data[:, 1], data[:, 2], color="blue")
-plt.plot([15, 15], [0, np.max(data[:, 2])], color="red")
-plt.plot([30, 30], [0, np.max(data[:, 2])], color="red")
-plt.ylabel("MSE")
-plt.xlabel("Epoch")
-plt.gca().set_yscale("log")
-# plt.xlim(0, 60)
-plt.title("phasecurve vector MSE")
-plt.savefig("phasecurvevectorMSE_full.png")
+ax[2].plot(data[:, 1], data[:, 2], color="blue", label="phasecurve\nvector MSE")
+ax[2].plot([15, 15], [0, np.max(data[:, 2])], color="red")
+ax[2].plot([30, 30], [0, np.max(data[:, 2])], color="red")
+ax[2].set_yscale("log")
+ax[2].set_ylim(0, np.max(data[:, 2]))
+ax[2].set_xlabel("Epoch")
+# ax[2].set_xlim(0, 60)
+ax[2].legend()
+
+
+
+plt.savefig("./mses_full.png")
 plt.show()
-
-
-
-
 
