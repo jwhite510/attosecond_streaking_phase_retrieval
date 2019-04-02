1cc5a947d3da713c4df00cf4f3eb9fe3db1f9a2a
jonathon <jonathonwhite5@gmail.com>
2019-04-02 16:04:42 -0400

    open and read data

--- v3/measured_trace/sample3/test_data.py
+++ v3/measured_trace/sample3/test_data.py
@@ -3,32 +3,38 @@ import matplotlib.pyplot as plt
 
 
 def retrieve_trace():
-    mat = []
+    trace = []
     for line in open("53as_trace.dat", "r"):
         line = line.rstrip()
         line = line.split("\t")
         line = [float(e) for e in line]
-        mat.append(line)
-    mat = np.transpose(np.array(mat))
+        trace.append(line)
+    trace = np.transpose(np.array(trace))
 
-    # delay_values =
+    delay_min = -5.47 #fs
+    delay_max = 5.44 #fs
+    delay = np.linspace(delay_min, delay_max, np.shape(trace)[1])
 
+    e_min = 50
+    e_max = 350
+    energy = np.linspace(e_min, e_max, np.shape(trace)[0])
 
+    # remove the last delay step so the delay axis is an even number for fourier transform
+    trace = trace[:, :-1]
+    delay = delay[:-1]
 
-    # normalize mat
-    mat = mat / np.max(mat)
+    # normalize trace
+    trace = trace / np.max(trace)
 
-    return mat
-
-
+    return delay, energy, trace
 
 
 if __name__ == "__main__":
 
-    trace = retrieve_trace()
+    delay, energy, trace = retrieve_trace()
 
     plt.figure(1)
-    plt.pcolormesh(trace, cmap="jet")
+    plt.pcolormesh(delay, energy, trace, cmap="jet")
 
     plt.show()
 
