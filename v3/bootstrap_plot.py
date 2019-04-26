7d82778792fee5b8a560ddf616e7e54eb32c4111
zom <jonathonwhite5@gmail.com>
2019-04-25 21:02:29 -0400

    bootstrap changes

--- v3/bootstrap_plot.py
+++ v3/bootstrap_plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pickle
 from xuv_spectrum import spectrum
 import phase_parameters.params as phase_params
+import unsupervised_retrieval
 spectrum_scaled = spectrum.fmat_hz_cropped * 1e-16
 
 
@@ -13,17 +14,17 @@ with open("noise_test7__.p", "rb") as file:
 with open("noise_test7___bootstrap.p", "rb") as file:
     data_bootstrap = pickle.load(file)
 
-
-
-run_sample = data_bootstrap["unsupervised_20"]
+key = "unsupervised_20"
+key = "ga_20"
+run_sample = data_bootstrap[key]
 
 
 # create axes
-fig = plt.figure()
+fig = plt.figure(figsize=(7,7))
 gs = fig.add_gridspec(2,1)
 
 total_samples = len(run_sample)
-ax = fig.add_subplot(gs[0,0])
+ax = fig.add_subplot(gs[1,0])
 phase_curve = None
 for thing in run_sample:
 
@@ -42,7 +43,7 @@ axtwin = ax.twinx()
 axtwin.plot(spectrum_scaled, mean_vals, color="green", label="Mean")
 first_plotted = False
 for x, this_std_val, this_mean_val in zip(spectrum_scaled[::8], standard_dev[::8], mean_vals[::8]):
-    scale_fac = 10
+    scale_fac = 1
     ymax_val = this_mean_val + (scale_fac * this_std_val/2)
     ymin_val = this_mean_val - (scale_fac * this_std_val/2)
     if not first_plotted:
@@ -65,13 +66,23 @@ ax.set_xlabel(r"$10^{16}$Hz")
 
 
 # plot the actual trace trace
-ax = fig.add_subplot(gs[1,0])
+ax = fig.add_subplot(gs[0,0])
 ax.pcolormesh(phase_params.delay_values_fs ,phase_params.K, data_retrievals["actual_values"]["measured_trace_20"], cmap="jet")
 ax.set_xlabel("time [fs]")
 ax.set_ylabel("Energy [eV]")
 
+# write the number of samples
+unsupervised_retrieval.normal_text(ax, (0.9, 1.1), "Samples = "+str(total_samples))
 
-fig.savefig("./bootstrap.png")
+if key[0] == "g":
+    ax.set_title("Genetic Algorithm")
+    fig.savefig("./bootstrap_ga.png")
+elif key[0] == "u":
+    ax.set_title("Unsupervised Learning")
+    fig.savefig("./bootstrap_ga.png")
+
+plt.show()
+
 
 
 
