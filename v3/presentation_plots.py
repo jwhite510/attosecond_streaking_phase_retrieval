0a77f1a24a72d48644b09aad49b077f1403e443e
jonathon <jonathonwhite5@gmail.com>
2019-05-01 21:53:34 -0400

    plot looks good

--- v3/presentation_plots.py
+++ v3/presentation_plots.py
@@ -30,10 +30,12 @@ def plot_images_fields_publication(traces_meas, traces_reconstructed, xuv_f, xuv
 
 
     # create new axes
-    fig = plt.figure(figsize=(10,7))
+    scaler =1.1 
+    fig = plt.figure(figsize=(10*scaler,7*scaler))
     gs = fig.add_gridspec(2,2)
     axes = dict()
     axes["input_trace"] = fig.add_subplot(gs[0,0])
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
     axes["reconstructed_trace"] = fig.add_subplot(gs[0,1])
     axes["xuv_t"] = fig.add_subplot(gs[1,1])
     axes["xuv_f"] = fig.add_subplot(gs[1,0])
@@ -68,12 +70,8 @@ def plot_images_fields_publication(traces_meas, traces_reconstructed, xuv_f, xuv
     axes["input_trace"].pcolormesh(params.delay_values_fs, params.K, traces_meas["trace"], cmap='jet')
     axes["input_trace"].set_xlabel(r"$\tau$ Delay [fs]")
     axes["input_trace"].set_ylabel("Energy [eV]")
-    if true_fields:
-        unsupervised_retrieval.normal_text(axes["input_trace"], (0.0, 1.0), "noisy trace")
-    else:
-        unsupervised_retrieval.normal_text(axes["input_trace"], (0.0, 1.0), "input trace")
-        # if cost_function == "trace":
-        #     unsupervised_retrieval.red_text(axes["input_trace"], (1.0, 1.0), "C")
+    unsupervised_retrieval.normal_text(axes["input_trace"], (0.5, 1.05), "Measured Trace", ha="center")
+
 
 
     # ..........................................
@@ -83,35 +81,35 @@ def plot_images_fields_publication(traces_meas, traces_reconstructed, xuv_f, xuv
     axes["reconstructed_trace"].set_xlabel(r"$\tau$ Delay [fs]")
     axes["reconstructed_trace"].set_ylabel("Energy [eV]")
     unsupervised_retrieval.normal_text(axes["reconstructed_trace"], (0.05, 0.05), "RMSE: "+"%.4f" % rmses["trace"])
-    if true_fields:
-        unsupervised_retrieval.normal_text(axes["reconstructed_trace"], (0.0, 1.0), "actual trace")
-    else:
-        unsupervised_retrieval.normal_text(axes["reconstructed_trace"], (0.0, 1.0), "generated trace")
-        # if cost_function == "trace":
-        #     unsupervised_retrieval.red_text(axes["reconstructed_trace"], (1.0, 1.0), "C")
+    unsupervised_retrieval.normal_text(axes["reconstructed_trace"], (0.5, 1.05), "Reconstructed Trace", ha="center")
 
 
     # xuv f
     fmat_hz = spectrum.fmat_cropped/sc.physical_constants['atomic unit of time'][0]*1e-17
     I_f = np.abs(xuv_f) ** 2
-    axes["xuv_f"].plot(fmat_hz, I_f, color="black")
+    axes["xuv_f"].plot(fmat_hz, I_f, color="black", label="Electron Spectrum")
     axes["xuv_f"].set_yticks([])
     axes["xuv_f"].set_xlabel("Frequency [$10^{17}$Hz]")
     axes["xuv_f"].set_xlim(np.min(fmat_hz), np.max(fmat_hz))
     axes["xuv_f"].set_ylim(np.min(I_f), 1.2*np.max(I_f))
+    axes["xuv_f"].set_ylabel("Intensity [Relative]")
     # plotting photon spectrum
-    axes["xuv_f"].plot(fmat_hz, np.abs(spectrum.Ef_photon[spectrum.indexmin:spectrum.indexmax]) ** 2, color="blue")
+    axes["xuv_f"].plot(fmat_hz, np.abs(spectrum.Ef_photon[spectrum.indexmin:spectrum.indexmax]) ** 2, color="blue", label="Photon Spectrum")
+    axes["xuv_f"].legend(loc=1)
+    unsupervised_retrieval.normal_text(axes["xuv_f"], (0.5, 1.05), "Predicted XUV spectrum", ha="center")
+    
 
 
-    if true_fields:
-        axes["xuv_f_phase"].text(0.0, 1.1, "actual XUV spectrum", backgroundcolor="white",
-                                         transform=axes["xuv_f_phase"].transAxes)
-    else:
-        axes["xuv_f_phase"].text(0.0, 1.1, "predicted XUV spectrum", backgroundcolor="white",
-                                         transform=axes["xuv_f_phase"].transAxes)
+    # if true_fields:
+    #     axes["xuv_f_phase"].text(0.0, 1.1, "actual XUV spectrum", backgroundcolor="white",
+    #                                      transform=axes["xuv_f_phase"].transAxes)
+    # else:
+    #     axes["xuv_f_phase"].text(0.0, 1.1, "predicted XUV spectrum", backgroundcolor="white",
+    #                                      transform=axes["xuv_f_phase"].transAxes)
 
     axes["xuv_f_phase"].tick_params(axis='y', colors='green')
     axes["xuv_f_phase"].plot(fmat_hz, -1*xuv_f_phase, color="green")
+    axes["xuv_f_phase"].set_ylabel("$\phi$ [Rad]", color="green")
 
 
     # xuv predicted
@@ -131,23 +129,37 @@ def plot_images_fields_publication(traces_meas, traces_reconstructed, xuv_f, xuv
     axes["xuv_t"].plot(tmat_as, I_t, color="black")
     #calculate FWHM
     fwhm, t1, t2, half_max = unsupervised_retrieval.calc_fwhm(tmat=tmat_as, I_t=I_t)
-    axes["xuv_t"].text(1.0, 0.9, "FWHM:\n %.2f [as]" % fwhm, color="red",
+    axes["xuv_t"].text(0.5, 0.5, "FWHM:\n %.2f [as]" % fwhm, color="black",
                             backgroundcolor="white", ha="center",
                             transform=axes["xuv_t"].transAxes)
     #plot FWHM
-    axes["xuv_t"].plot([t1, t2], [half_max, half_max], color="red", linewidth=2.0)
+    # axes["xuv_t"].plot([t1, t2], [half_max, half_max], color="red", linewidth=2.0)
+    
+    # draw arrows showing fwhm
+    arrow_dx = np.abs(0.5*t1)
+    arrow_length = 25
+    axes["xuv_t"].arrow(t1 - arrow_dx - arrow_length, half_max, arrow_dx, 0.0,
+            width=0.020, head_width=100, head_length=arrow_length , 
+            color="black")
+    axes["xuv_t"].arrow(t2 + arrow_dx + arrow_length, half_max, -arrow_dx, 0.0,
+            width=0.020, head_width=100, head_length=arrow_length , 
+            color="black")
+
+
     axes["xuv_t"].set_yticks([])
     axes["xuv_t"].set_xlim(-150, 260)
     axes["xuv_t"].set_ylim(0, 1.2*np.max(I_t))
     axes["xuv_t"].set_xlabel("time [as]")
+    axes["xuv_t"].set_ylabel("Intensity [Relative]")
     # axes["xuv_t"].set_xlim(-200, 300)
+    unsupervised_retrieval.normal_text(axes["xuv_t"], (0.5, 1.05), "Predicted XUV $I(t)$", ha="center")
 
-    if true_fields:
-        axes["xuv_t"].text(0.0, 1.1, "actual XUV $I(t)$", backgroundcolor="white",
-                                     transform=axes["xuv_t"].transAxes)
-    else:
-        axes["xuv_t"].text(0.0, 1.1, "predicted XUV $I(t)$", backgroundcolor="white",
-                                     transform=axes["xuv_t"].transAxes)
+    # if true_fields:
+    #     axes["xuv_t"].text(0.0, 1.1, "actual XUV $I(t)$", backgroundcolor="white",
+    #                                  transform=axes["xuv_t"].transAxes)
+    # else:
+    #     axes["xuv_t"].text(0.0, 1.1, "predicted XUV $I(t)$", backgroundcolor="white",
+    #                                  transform=axes["xuv_t"].transAxes)
     fig.savefig("./publication_plot.png")
 
 
