c0aa8f57f4ed89ca4b819816d00f597c2db725d6
jonathon <jonathonwhite@protonmail.com>
2019-08-19 12:36:31 -0400

    get number of samples more efficiently, make sample size smaller because this was crashing on newton

--- v3/network3.py
+++ v3/network3.py
@@ -473,9 +473,7 @@ class GetData():
         self.test_filename = 'test3.hdf5'
 
         hdf5_file = tables.open_file(self.train_filename, mode="r")
-        attstraces = hdf5_file.root.noise_trace[:, :]
-        self.samples = np.shape(attstraces)[0]
-
+        self.samples = hdf5_file.root.noise_trace.shape[0]
         hdf5_file.close()
 
     def next_batch(self):
@@ -1388,6 +1386,6 @@ def calc_bootstrap_error(recons_trace_in, input_trace_in):
     return bootstrap_loss, bootstrap_indexes_ph
 
 if __name__ == "__main__":
-    phase_net_train = PhaseNetTrain(modelname='HHH_sample4_noise_resistant_network_cosphase_cosplot_cosradians')
+    phase_net_train = PhaseNetTrain(modelname='LLL_sample4_noise_resistant_network_various_noise_per_trace3')
     phase_net_train.supervised_learn()
 
