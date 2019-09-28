3243237c9c86862bc6913fda0ff68e3657f7030d
jonathon <jonathonwhite5@gmail.com>
2019-09-27 20:21:31 -0400

    everything working, makes sense, plotting cross sections and electron/photon spectrum integrals

--- v3/cross_section_test.py
+++ v3/cross_section_test.py
@@ -63,21 +63,26 @@ def ev_to_p(vector):
     ev_to_p(vector [eV])
     return vector [a.u. momenrum]
     """
-    vector =  np.array(xuv_spectrum.cross_section_ev) * sc.electron_volt # joules
+    vector =  np.array(vector) * sc.electron_volt # joules
     vector = vector / sc.physical_constants['atomic unit of energy'][0]  # a.u. energy
     vector = np.sqrt(2 * vector) # a.u. momentum
 
     return vector
 
+def ev_to_wl(vector):
+    """
+    ev_to_joules(vector [eV])
+    return vector [joules energy]
+    """
+    vector_joules =  np.array(vector) * sc.electron_volt # joules
+    vector_hz = vector_joules / sc.h # Hz
+    # c = lambda * f
+    wl_lambda = sc.c / vector_hz
+    return wl_lambda
 
 
 
-
-
-if __name__ == "__main__":
-
-    # multiply_by_cross_section()
-
+def multiply_by_cross_section_integrate():
 
     # Fourier transform
     # xuv_spectrum.tmat
@@ -111,19 +116,70 @@ if __name__ == "__main__":
     plt.plot(np.squeeze(p), integral_photon, "b")
     plt.title(r"$| \int_{-\infty}^{\infty} E_{xuv}^{photon}(t) \sqrt{\sigma(p)} e^{i(\frac{p^2}{2})t} dt |^2$")
     plt.xlabel("momentum p [a.u.]")
+    plt.gcf().savefig("./photon_integral.png")
 
     plt.figure(4)
     plt.plot(np.squeeze(p), integral_electron, "b")
     plt.title(r"$| \int_{-\infty}^{\infty} E_{xuv}^{electron}(t) e^{i(\frac{p^2}{2})t} dt |^2$")
     plt.xlabel("momentum p [a.u.]")
-    # plt.plot(np.squeeze(p), cross_section_p)
-    # plt.plot(np.squeeze(p), (integral_electron/cross_section_p)/np.max(integral_electron/cross_section_p))
-    # plt.plot(np.squeeze(p), integral_photon, "r--")
+    plt.gcf().savefig("./electron_integral.png")
+
+    # plot cross section
+    henke_cross_section_ev, henke_cross_section_f2 = open_he_cross_section()
+    henke_cross_section_p = ev_to_p(henke_cross_section_ev)
+    wl_lambda = ev_to_wl(henke_cross_section_ev)
+    r_e = sc.physical_constants['classical electron radius'][0]
+    # calcualte cross section
+
+    # henke_cross_section_wl_lambda = # meters
+    henke_cross_section_sigma = 2 * r_e * wl_lambda * henke_cross_section_f2
+    # interpolate henke cross section onto p values
+
+    henke_interp = scipy.interpolate.interp1d(henke_cross_section_p, henke_cross_section_sigma)
+    henk_vals_interp_sigma_p = henke_interp(np.squeeze(p))
+    henk_vals_interp_sigma_p = henk_vals_interp_sigma_p / np.max(henk_vals_interp_sigma_p)
+
+    plt.figure(5)
+    plt.plot(np.squeeze(p), cross_section_p, "b-",label="cross section\n Jie gave me ")
+    plt.plot(np.squeeze(p), henk_vals_interp_sigma_p, "r-", label="cross section\ncalculated from data on henke.lbl.gov\n"+r"$\sigma = (2\cdot r_e\cdot  \lambda \cdot f_2)$")
+    plt.ylabel("$\sigma(p)$")
+    plt.xlabel("momentum p[a.u.]")
+    l = plt.gca().legend()
+    l.get_texts()[0].set_color("blue")
+    l.get_texts()[1].set_color("red")
+    plt.gcf().savefig("./cross_sections.png")
 
 
-    # plt.figure(1)
-    # plt.plot(xuv_spectrum.tmat, np.real(xuv_Et_electron))
-    # plt.show()
-    # xuv_spectrum.Ef_photon
     plt.show()
+
+
+def open_he_cross_section():
+
+    with open("./xuv_spectrum/sample4/henke_data/he.txt", "r") as file:
+        f2_vals = []
+        E_eV_vals = []
+        for line in list(file.readlines())[1:]:
+            f2_val = line.split()[2]
+            f2_vals.append(f2_val)
+            E_eV_val = line.split()[0]
+            E_eV_vals.append(E_eV_val)
+
+        E_eV_vals = [float(e) for e in E_eV_vals]
+        f2_vals = [float(e) for e in f2_vals]
+
+        E_eV_vals = np.array(E_eV_vals)
+        f2_vals = np.array(f2_vals)
+
+    return E_eV_vals, f2_vals
+
+
+
+if __name__ == "__main__":
+    multiply_by_cross_section_integrate()
+
+    # multiply_by_cross_section()
+
+    # open_he_cross_section()
+
+
 
